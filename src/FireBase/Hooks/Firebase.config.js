7e28ac284b1ab80b27ffdf6ff2fbf1213c7bba37
redoywebhero <redoyhossin5984@gmail.com>
2022-10-26 11:18:18 +0600

    set environment

--- src/FireBase/Hooks/Firebase.config.js
+++ src/FireBase/Hooks/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB3X_tdigrvbln_c_Q2UGQYLXYgyV3nkRE",
-    authDomain: "learn-code-494e1.firebaseapp.com",
-    projectId: "learn-code-494e1",
-    storageBucket: "learn-code-494e1.appspot.com",
-    messagingSenderId: "883017443133",
-    appId: "1:883017443133:web:73a39f057b72749d508754"
+    apiKey:process.env.REACT_APP_apiKey,
+    authDomain:process.env.REACT_APP_authDomain,
+    projectId:process.env.REACT_APP_projectId,
+    storageBucket:process.env.REACT_APP_storageBucket,
+    messagingSenderId:process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
--- src/pages/blogs/Blog.js
+++ src/pages/blogs/Blog.js
@@ -1,18 +1,10 @@
-import { Button, Tooltip } from 'flowbite-react';
 import React from 'react';
 
 const Blog = () => {
     return (
         <div>
             
-            <Tooltip
-                content="Tooltip content"
-                placement="top"
-            >
-                <Button>
-                    Tooltip top
-                </Button>
-            </Tooltip>
+           
 
 
         </div>
